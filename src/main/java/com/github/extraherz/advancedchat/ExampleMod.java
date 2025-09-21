60ead52c93def146429c1f521919a2dd339e2eae
GitHub Action <action@github.com>
2025-09-21 23:35:14 +0000

    Initialize template repository proper
    Set package name to com.github.extraherz.advancedchat
    Set project name to Advanced-Chat
    Set modid to advancedchat

--- src/main/java/com/github/extraherz/advancedchat/ExampleMod.java
+++ src/main/java/com/github/extraherz/advancedchat/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.extraherz.advancedchat;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "advancedchat", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
